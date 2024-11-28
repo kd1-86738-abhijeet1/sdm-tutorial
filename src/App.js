d13f5f7255be23799f0039475cb52fce61788f50
Abhijeet <abhijeetpkalbhor@gmail.com>
2024-11-28 22:54:47 +0530

    paragraph change

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
       <h2 className="text-center text-info">This is my first react application</h2>
+      <p className="text-center text-danger">Hii this is a new changes</p>
     </>
   );
 }
